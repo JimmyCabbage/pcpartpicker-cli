1e833b0e626e8cb461ec2eacedd290f596f3ca9d
Ryan Rhee <kapowcabbage096@outlook.com>
2023-09-03 18:49:20 -0700

    Move up version to 1.0 in maven & Main

--- src/main/java/com/ryanrhee/Main.java
+++ src/main/java/com/ryanrhee/Main.java
@@ -12,7 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
-@Command(name = "pcpartpicker-cli", mixinStandardHelpOptions = true, version = "pcpartpicker-cli 0.1")
+@Command(name = "pcpartpicker-cli", mixinStandardHelpOptions = true, version = "pcpartpicker-cli 1.0")
 public class Main implements Runnable {
     public static void main(String[] args) {
         int exitCode = new CommandLine(new Main()).execute(args);
